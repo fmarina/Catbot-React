c7a3e765e974e7c6bd114ab59e72a567053ff7c0
Marina <marinafaenze@hotmail.com>
2020-07-19 17:38:25 -0300

    Cat and Lottie component created

---
+++ src/pages/Home/components/Cat/Cat.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Cat.css';
+import animationCat from './cat-data.json';
+import Lottie from 'react-lottie';
+
+const Cat = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationCat,
+    };
+
+    return (
+        <div className="cat-container">
+            <Lottie options={defaultOptions}/>
+        </div>
+    );
+}
+
+export default Cat;
