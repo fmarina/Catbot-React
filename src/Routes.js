38865538b30eb69156481ff35db39d908da445ba
Marina <marinafaenze@hotmail.com>
2020-07-20 17:36:01 -0300

    Import chat component to Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Chat from './pages/Chat/'
+import Chat from './pages/Chat/Chat';
 
 const Routes = () => {
+
     return (
         <Router>
             <Switch>
                 <Route exact path="/"><Home/></Route>
-                <Route path="/chat"></Route>
+                <Route path="/chat"><Chat/></Route>
             </Switch>
         </Router>
     );
