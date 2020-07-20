77b0bb7ef00ec5a40f16f2525f28b3139b7b0b53
Marina <marinafaenze@hotmail.com>
2020-07-20 19:27:07 -0300

    Style added to Home component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,16 @@ const Home = () => {
     
     return (
         <div>
-            <Cat />
+            <div className="home-catbot-container">
+                <div className="home-catbot-content">
+                    <Cat />
+                    <div className="home-catbot-greeting">
+                        <h1>Hola humano!</h1>
+                        <label>¿Quieres charlar?</label>
+                        <label>Haz click sobre mi para comenzar</label>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
