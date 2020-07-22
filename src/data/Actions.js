aa5ce5a06e571c1a0de70a62a788c14487ce3cdb
Marina <marinafaenze@hotmail.com>
2020-07-22 17:08:49 -0300

    Actions doing and aboutMe data created

---
+++ src/data/Actions.js
@@ -0,0 +1,15 @@
+export const doing = [
+    { msg : ["Estoy chateando contigo, ¿Qué más haría? 🤨"] },
+    { msg : ["Por ir a dormir una siesta al lado de la ventana 😴"] },
+    { msg : ["Afilando mis uñitas en el sillón 😊"] },
+    { msg : ["Acá en casa. Respetando la cuarentena! 😷"] }
+];
+
+export const aboutMe = [
+    { msg : ["Soy el gato con mas seguidores en instagram... Okey, no."] },
+    { msg : ["El atún es mi comdia favorita. Pero no me lo dan muy seguido"] },
+    { msg : ["Mi mejor amigo es un 🐶. Es mi vecino. Pasamos todas las tardes juntos"] },
+    { msg : ["Odio el agua 💧"] },
+    { msg : ["Me encanta comer 🍉 en verano"] },
+    { msg : ["Me gusta dormir la siesta"] }
+];
