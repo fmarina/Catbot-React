613899dc3382614c4534892c685af79c3887c0e1
Marina <marinafaenze@hotmail.com>
2020-07-22 12:03:42 -0300

    Select Component created and styled

---
+++ src/pages/Chat/Select/Select.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Select.css';
+
+const Select = () => {
+    return (
+        <div className="selector-container">
+            <div className="selector-content">
+                <div className="selector-options">
+                    <label>option 1</label>
+                    <label>option 2</label>
+                    <label>option 3</label>
+                </div>
+            </div>
+        </div>
+    );    
+}
+
+export default Select;
