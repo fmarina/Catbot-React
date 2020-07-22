8983b391028c0c09381ec40f7433a609e476e41d
Marina <marinafaenze@hotmail.com>
2020-07-21 21:32:05 -0300

    set conditional render between CatItem and UserItem

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Chat.css';
 import CatItem from './CatItem/CatItem';
 import UserItem from './UserItem/UserItem';
 import InputChat from './InputChat/InputChat';
 
 const Chat = () => {
+
+    const [msg, setMsg] = useState({});
+    const [chat, setChat] = useState([
+        {
+            id : 0,
+            emmiter : "Cat",
+            msg : ["Hola!", "Cómo es tu nombre?"]
+        }
+    ]);
+
     return (
         <div className="chat-container">
             <div className="chat-content">
                 <div className="chatbox-chat">
                     <div className="chatbox-container-body">
-                        <CatItem text="Hola, bienvenido. Miau." />
-                        <UserItem text="Gracias por querer ayudarme"/>                        
+                        {   
+                            chat.map((message, i) => 
+                                message.emmiter === "Cat"
+                                ? <CatItem key={i} text={message.msg} />
+                                : <UserItem key={i} text={message.msg} />
+                            )       
+                        }                        
                     </div>
                     <div className="chatbox-container-input">
                             <InputChat />
