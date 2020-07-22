c672be82303b0c96bd395e1e9a81d55331bd6d27
Marina <marinafaenze@hotmail.com>
2020-07-22 11:05:03 -0300

    value property added to component

--- src/pages/Chat/InputChat/InputChat.js
+++ src/pages/Chat/InputChat/InputChat.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './InputChat.css';
 
-const InputChat = ({getMeMessage, sendMessage}) => {
+const InputChat = ({getMeMessage, sendMessage, msg}) => {
     return (
         <form onSubmit={(e) => sendMessage(e)} className="chatbox-inputchat-container">
             <input 
                 type="text" 
                 placeholder="Ingrese nombre"
                 onChange={(e) => getMeMessage(e.target.value)}
+                value={msg.msg}
             />
             <button type="submit" />
         </form>
