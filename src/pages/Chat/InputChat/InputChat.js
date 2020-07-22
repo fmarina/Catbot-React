43ac06d61075421a7c560153de50316f02336011
Marina <marinafaenze@hotmail.com>
2020-07-22 18:22:28 -0300

    conditional placeholder and disabled property implemented

--- src/pages/Chat/InputChat/InputChat.js
+++ src/pages/Chat/InputChat/InputChat.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './InputChat.css';
 
-const InputChat = ({getMeMessage, sendMessage, msg}) => {
+const InputChat = ({getMeMessage, sendMessage, msg, chat}) => {
     return (
         <form onSubmit={(e) => sendMessage(e)} className="chatbox-inputchat-container">
             <input 
                 type="text" 
-                placeholder="Ingrese nombre"
-                onChange={(e) => getMeMessage(e.target.value)}
                 value={msg.msg}
+                onChange={(e) => getMeMessage(e.target.value)}
+                placeholder={ (chat.length >= 3) ? "" : "Ingrese nombre"}
+                disabled={ (chat.length >= 3) ? true : false}
             />
             <button type="submit" />
         </form>
