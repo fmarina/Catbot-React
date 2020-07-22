1d6f462a705adc32187af9482c8f893607dc7354
Marina <marinafaenze@hotmail.com>
2020-07-22 11:05:29 -0300

    Fade reactreveal animation implemented

--- src/pages/Chat/CatItem/CatItem.js
+++ src/pages/Chat/CatItem/CatItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './CatItem.css';
 import catAvatar from '../../../assets/images/cat-avatar.png';
+import Fade from 'react-reveal/Fade';
 
 const CatItem = ({text}) => {
     
     return (
         <div className="cat-item-container">
+        <Fade cascade>
             <img src={catAvatar} alt="cat avatar" />
             <div className="cat-item-messages">
                 { text.map((txt, i) => <label key={i}>{txt}</label> ) } 
             </div>
+        </Fade>        
         </div>
     );
 }
--- src/pages/Chat/UserItem/UserItem.js
+++ src/pages/Chat/UserItem/UserItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './UserItem.css';
 import userAvatar from '../../../assets/images/user-avatar.png';
+import Fade from 'react-reveal/Fade';
 
 const UserItem = ({text}) => {
     return (
         <div className="user-item-container">
+        <Fade cascade>
             <div className="user-item-messages">
                 <label>{text}</label>
             </div>
-            <img src={userAvatar} alt="user avatar" />     
+            <img src={userAvatar} alt="user avatar" />
+        </Fade>
         </div>
     );
 }
