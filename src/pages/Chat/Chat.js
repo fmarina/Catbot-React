1d40433e3b63fa3893f6ee1f5371d1f05f9035a0
Marina <marinafaenze@hotmail.com>
2020-07-21 21:07:31 -0300

    fix responsive design

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Chat.css';
 import CatItem from './CatItem/CatItem';
 import UserItem from './UserItem/UserItem';
+import InputChat from './InputChat/InputChat';
 
 const Chat = () => {
     return (
@@ -10,7 +11,10 @@ const Chat = () => {
                 <div className="chatbox-chat">
                     <div className="chatbox-container-body">
                         <CatItem />
-                        <UserItem />
+                        <UserItem />                        
+                    </div>
+                    <div className="chatbox-container-input">
+                            <InputChat />
                     </div>
                 </div>
             </div>
--- src/pages/Chat/InputChat/InputChat.js
+++ src/pages/Chat/InputChat/InputChat.js
@@ -3,15 +3,13 @@ import './InputChat.css';
 
 const InputChat = () => {
     return (
-        <div>
-            <form className="chatbox-inputchat-container">
-                <input 
-                    type="text" 
-                    placeholder="Ingrese nombre"
-                />
-                <button type="submit" />
-            </form>
-        </div>
+        <form className="chatbox-inputchat-container">
+            <input 
+                type="text" 
+                placeholder="Ingrese nombre"
+            />
+            <button type="submit" />
+        </form>
     );
 }
 
