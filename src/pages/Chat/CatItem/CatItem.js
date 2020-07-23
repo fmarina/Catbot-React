b1b4efc67e3330aa2b96479ec9acea4b39bc5d2c
Marina <marinafaenze@hotmail.com>
2020-07-23 11:21:33 -0300

    render div content for meme

--- src/pages/Chat/CatItem/CatItem.js
+++ src/pages/Chat/CatItem/CatItem.js
@@ -3,13 +3,21 @@ import './CatItem.css';
 import catAvatar from '../../../assets/images/cat-avatar.png';
 
 const CatItem = ({text}) => {
+    let imgUrl = "";
+    if (text[0] === "meme") imgUrl = text[1];
     
     return (
         <div className="cat-item-container">
             <img src={catAvatar} alt="cat avatar" />
             <div className="cat-item-messages">
-                { text.map((txt, i) => <label key={i}>{txt}</label> ) }
+                { imgUrl === ""  && text.map((txt, i) => <label key={i}>{txt}</label> ) }
             </div>
+            {
+                imgUrl !== "" &&
+                    <div className="cat-item-meme-container">
+                        <img src={imgUrl} alt="a cat meme"/>
+                    </div>
+            }            
         </div>
     );
 }
